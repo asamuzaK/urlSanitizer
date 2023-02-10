062c8d73eb59526ca292e76b6b085ffdb369a1c0
asamuzaK (Kazz) <kazz@asamuzak.jp>
2023-02-10 10:09:18 +0900

    Add replacement file

---
+++ repl/file-reader.js
@@ -0,0 +1,13 @@
+/**
+ * file-reader.js
+ */
+
+/* instance */
+const FileReader = window.FileReader;
+const fileReader = new FileReader();
+
+/* export */
+export {
+  fileReader as default,
+  FileReader
+};
