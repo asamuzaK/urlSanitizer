5f451178e47da8df7a445ea38d9cb37374df2261
asamuzaK (Kazz) <kazz@asamuzak.jp>
2023-02-10 16:15:11 +0900

    Update file-reader.js

--- repl/file-reader.js
+++ repl/file-reader.js
@@ -2,11 +2,9 @@
  * file-reader.js
  */
 
-/* instance */
 const FileReader = window.FileReader;
 const fileReader = new FileReader();
 
-/* export */
 export {
   fileReader as default,
   FileReader
