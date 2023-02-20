59d23ef29c3444203c22c56983036f4a52fc857e
asamuzaK (Kazz) <kazz@asamuzak.jp>
2023-02-20 21:41:31 +0900

    Create dompurify.test.js

---
+++ test/dompurify.test.js
@@ -0,0 +1,18 @@
+/**
+ * dompurify.test.js
+ */
+
+/* api */
+import { assert } from 'chai';
+import { describe, it } from 'mocha';
+
+/* test */
+import domPurify from '../src/mjs/dompurify.js';
+
+describe('DOMPurify', () => {
+  describe('domPurify', () => {
+    it('should have methods', () => {
+      assert.isFunction(domPurify.sanitize, 'sanitize');
+    });
+  });
+});
