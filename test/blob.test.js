dfc9ecb9ceff53c263dda5e4d509520e2c9316dd
asamuzaK (Kazz) <kazz@asamuzak.jp>
2023-07-22 14:32:34 +0900

    Update blob.test.js

--- test/blob.test.js
+++ test/blob.test.js
@@ -4,14 +4,13 @@ import { describe, it } from 'mocha';
 
 describe('test for blob', () => {
   it('should get blob', async () => {
-    const src = '<svg></svg>';
-    const file = new Blob([src], {
+    const file = new Blob(['<svg></svg>'], {
       type: 'image/svg+xml'
     });
     const url = URL.createObjectURL(file);
     const res = await fetch(url);
     URL.revokeObjectURL(url);
     const blob = await res.blob(); // does not resolve
-    console.log(blob instanceof Blob); // nothing logged
+    console.log(blob.size); // nothing logged
   });
 });
