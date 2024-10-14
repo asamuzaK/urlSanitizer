cc828c3fc8b9e488fe7ed629c712fb4bf7f80cdd
asamuzaK (Kazz) <kazz@asamuzak.jp>
2024-10-14 09:02:54 +0900

    Update lib-util.js

--- repl/lib-util.js
+++ repl/lib-util.js
@@ -2,10 +2,10 @@
  * lib-util.js
  */
 
-import textChars from '../lib/file/text-chars.json' assert {
+import textChars from '../lib/file/text-chars.json' with {
   type: 'json'
 };
-import uriSchemes from '../lib/iana/uri-schemes.json' assert {
+import uriSchemes from '../lib/iana/uri-schemes.json' with {
   type: 'json'
 };
 
