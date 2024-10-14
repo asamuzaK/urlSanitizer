e61b525c94bed275d2a585048306b15f43d7474c
asamuzaK (Kazz) <kazz@asamuzak.jp>
2024-10-14 09:02:58 +0900

    Update constant.js

--- src/mjs/constant.js
+++ src/mjs/constant.js
@@ -12,7 +12,7 @@ export const REG_B64 = /^[\w+/=-]+$/;
 export const REG_CHARSET = /^charset=([\w#&.;-]+)$/;
 export const REG_DATA_URL = /data:[\w#&+./;=-]*,/;
 export const REG_DATA_URL_B64 = /data:[\w#&+./;=^]*base64,[\w+/=-]+/i;
-export const REG_DATA_URL_G = /data:[\w#&+./;=-]*,[^"]+/g;
+export const REG_DATA_URL_G = /data:[\w#&+./;=-]*,[^\x22]+/g;
 export const REG_NUM_REF = /&#(x(?:00)?[\dA-F]{2}|0?\d{1,3});?/gi;
 export const REG_MIME_DOM =
   /^(?:application\/(?:[\w#&.;-]+\+)?x|image\/svg\+x|text\/(?:ht|x))ml;?/;
@@ -21,5 +21,5 @@ export const REG_SCHEME = /^[a-z][\da-z+.-]*$/;
 export const REG_SCHEME_EXT = /^(?:ext|web)\+[a-z]+$/;
 export const REG_SCRIPT = /(?:java|vb)script/;
 export const REG_SCRIPT_BLOB = /(?:java|vb)script|blob/;
-export const REG_TAG_QUOT = /%(?:2[27]|3[CE])|[<>"']/;
+export const REG_TAG_QUOT = /%(?:22|27|3C|3E)|[\x22\x27<>]/;
 export const REG_URL_ENC = /^%[\dA-F]{2}$/i;
