0300ef01b33b3515bc36b8aec3a56df34ee4d6c9
asamuzaK (Kazz) <kazz@asamuzak.jp>
2024-10-26 15:33:37 +0900

    Update lib-util.d.ts

--- types/mjs/lib-util.d.ts
+++ types/mjs/lib-util.d.ts
@@ -1,3 +1,7 @@
-export function parseJson(url: string): any;
-export const textChars: Array<number>;
-export const uriSchemes: Array<string>;
+import textChars from '../lib/file/text-chars.json' with {
+    type: 'json'
+};
+import uriSchemes from '../lib/iana/uri-schemes.json' with {
+    type: 'json'
+};
+export { textChars, uriSchemes };
