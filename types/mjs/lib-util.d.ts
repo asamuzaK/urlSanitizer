a63d6abab9abecace0734a66e7047465e4d04f07
asamuzaK (Kazz) <kazz@asamuzak.jp>
2025-10-05 09:32:02 +0900

    v2.0.10

--- types/mjs/lib-util.d.ts
+++ types/mjs/lib-util.d.ts
@@ -1,3 +1,3 @@
-export function parseJson(url: string): any;
+export function parseJson(url: string): string | any[] | object;
 export const textChars: Array<number>;
 export const uriSchemes: Array<string>;
