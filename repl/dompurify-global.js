e642e38f4446b102ce21e336dfa872774271b8a2
asamuzaK (Kazz) <asamuzaK@users.noreply.github.com>
2026-05-16 10:48:30 +0900

    Update dompurify-global.js

--- repl/dompurify-global.js
+++ repl/dompurify-global.js
@@ -2,8 +2,20 @@
  * dompurify.js
  */
 
-const { DOMPurify } = globalThis;
+const domPurify = {
+  sanitize: (...args) => {
+    if (globalThis?.DOMPurify) {
+      return globalThis.DOMPurify.sanitize(...args);
+    }
+
+    const msg = [
+      'DOMPurify is not available.',
+      'Ensure DOMPurify is exposed globally (e.g., window.DOMPurify).'
+    ];
+    throw new Error(msg.join(' '));
+  }
+};
 
 export {
-  DOMPurify as domPurify
+  domPurify
 };
