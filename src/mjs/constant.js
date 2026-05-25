6b2267d021bf1deeb407377ac89e4b3d19e90203
asamuzaK (Kazz) <asamuzaK@users.noreply.github.com>
2026-05-26 01:36:25 +0900

    Update constant.js

--- src/mjs/constant.js
+++ src/mjs/constant.js
@@ -5,7 +5,7 @@
 /* numeric */
 export const CHUNK_SIZE = 8 * 1024;
 export const HEX = 16;
-export const MAX_BLOB_SIZE = 32 * 1024 * 1024;
+export const MAX_BLOB_SIZE = 16 * 1024 * 1024;
 export const TYPE_FROM = 8;
 export const TYPE_TO = -1;
 
