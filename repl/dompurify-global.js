6cb4cc114228d6ad9e08de6652c5c6b7b2957d4a
asamuzaK (Kazz) <asamuzaK@users.noreply.github.com>
2026-05-29 08:42:30 +0900

    Proxify

--- repl/dompurify-global.js
+++ repl/dompurify-global.js
@@ -1,15 +1,37 @@
 /**
- * dompurify.js
+ * dompurify.js (for wo-dompurify build)
  */
 
-const domPurify = globalThis.DOMPurify || globalThis.window?.DOMPurify;
+/* constants */
+const PROPERTIES = Object.freeze([
+  'addHook',
+  'removeHook',
+  'removeAllHooks',
+  'setConfig'
+]);
 
-if (!domPurify) {
-  const msg = [
-    'DOMPurify is not defined.'
-    'Please ensure DOMPurify is exposed globally (e.g., window.DOMPurify).'
-  ]
-  throw new Error(msg.join(' '));
-}
-
-export { domPurify };
+const hookQueue = [];
+export const domPurify = new Proxy({}, {
+  get(target, prop) {
+    const purifier = globalThis.DOMPurify || globalThis.window?.DOMPurify;
+    if (purifier) {
+      if (hookQueue.length) {
+        for (const { prop: p, args } of hookQueue) {
+          if (typeof purifier[p] === 'function') {
+            purifier[p](...args);
+          }
+        }
+        hookQueue.length = 0;
+      }
+      const value = purifier[prop];
+      return typeof value === 'function' ? value.bind(purifier) : value;
+    }
+    return (...args) => {
+      if (PROPERTIES.includes(prop)) {
+        hookQueue.push({ prop, args });
+        return;
+      }
+      throw new Error('DOMPurify is not available. Ensure DOMPurify is exposed globally (e.g., window.DOMPurify).');
+    };
+  }
+});
