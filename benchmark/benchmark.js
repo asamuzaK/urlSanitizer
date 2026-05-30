1d6f8242e536e5fb2cf00152f83181f5e852542b
asamuzaK (Kazz) <asamuzaK@users.noreply.github.com>
2026-05-30 09:07:14 +0900

    Update benchmark.js

--- benchmark/benchmark.js
+++ benchmark/benchmark.js
@@ -12,14 +12,16 @@ const normalUrl = 'https://www.example.com/path/to/page?query=1#top';
 const xssUrl = 'javascript:alert("XSS")';
 const complexDataUrl = 'data:text/html;base64,PGRpdj48c2NyaXB0PmFsZXJ0KDEpOzwvc2NyaXB0PjwvZGl2PjxwIG9uY2xpY2s9ImFsZXJ0KDIpIj48L3A+';
 
+const sanitizeOpt = { allow: ['data'] };
+
 group('1. Normal HTTP URL', () => {
-  bench('url-sanitizer', () => sanitizeURLSync(normalUrl, { allow: ['data'] }));
+  bench('url-sanitizer', () => sanitizeURLSync(normalUrl, sanitizeOpt));
   bench('@braintree/sanitize-url', () => braintreeSanitize(normalUrl));
   bench('strict-url-sanitise', () => strictUrlSanitise(normalUrl));
 });
 
 group('2. XSS URL', () => {
-  bench('url-sanitizer', () => sanitizeURLSync(xssUrl, { allow: ['data'] }));
+  bench('url-sanitizer', () => sanitizeURLSync(xssUrl, sanitizeOpt));
   bench('@braintree/sanitize-url', () => braintreeSanitize(xssUrl));
   bench('strict-url-sanitise', () => {
     try { strictUrlSanitise(xssUrl); } catch (e) {}
@@ -27,7 +29,7 @@ group('2. XSS URL', () => {
 });
 
 group('3. Complex Data URL', () => {
-  bench('url-sanitizer', () => sanitizeURLSync(complexDataUrl, { allow: ['data'] }));
+  bench('url-sanitizer', () => sanitizeURLSync(complexDataUrl, sanitizeOpt));
   bench('@braintree/sanitize-url', () => braintreeSanitize(complexDataUrl));
   bench('strict-url-sanitise', () => {
     try { strictUrlSanitise(complexDataUrl); } catch (e) {}
