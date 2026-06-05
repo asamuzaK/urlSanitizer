74dc8fd88e8220f1aacee1840dcfd00955350a9f
asamuzaK (Kazz) <asamuzaK@users.noreply.github.com>
2026-06-06 08:09:23 +0900

    v4.1.1

--- types/mjs/uri-util.d.ts
+++ types/mjs/uri-util.d.ts
@@ -8,6 +8,6 @@ export function getURLEncodedString(str: string): string;
 export function escapeURLEncodedHTMLChars(ch: string): string;
 export function parseBase64(data: string): string;
 export function replaceNumCharRef(match: string, value: string): string;
-export function parseURLEncodedNumCharRef(str: string, nest?: number): string;
-export function createDataURLFromBlob(blob: Blob, maxBlobSize?: number): Promise<string | null>;
 export function trimTrailingEmptyQueryAndHash(url: string): string;
+export function parseURLEncodedNumCharRef(str: string, nest?: number): string;
+export function convertBlobToDataURL(blob: Blob, maxSize?: number): Promise<string | null>;
